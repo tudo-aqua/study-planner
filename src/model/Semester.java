5ec2c8f7295a0a77ffb6f5b98a79bfb07ebbf9f6
Christian Riest <christian.riest@cs.tu-dortmund.de>
2021-01-28 23:47:19 +0100

    Klasse Semester implementiert

--- src/model/Semester.java
+++ src/model/Semester.java
@@ -1,35 +1,125 @@
 package model;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
 
 import java.time.LocalDate;
 import java.util.Collection;
 
+/**
+ * Diese Klasse repräsentiert ein Semester innerhalb eines Studienganges.
+ */
 public class Semester {
 
+	/**
+	 * Der Name des Semesters, z.B. Wintersemester 2020/2021.
+	 */
 	private StringProperty name;
 
+	/**
+	 * Das Datum, an dem das Semester beginnt.
+	 */
 	private ObjectProperty<LocalDate> startDate;
 
+	/**
+	 * Das Datum, an dem das Semester endet.
+	 */
 	private ObjectProperty<LocalDate> endDate;
 
-	private Collection<Module> modules;
+	/**
+	 * Liste aller Module, die in dem Semester belegt werden/wurden.
+	 */
+	private ListProperty<Module> modules;
 
+	/**
+	 * Konstruktor zum erzeugen eines Semesters.
+	 * @param name Der Name des Semesters.
+	 * @param startDate Das Startdatum des Semesters.
+	 * @param endDate Das Enddatum des Semester.
+	 */
 	public Semester(String name, LocalDate startDate, LocalDate endDate) {
-
+		//Alle Attribute der Klasse werden mit den übergebenen
+		//Werten bzw. einer leeren Liste an Modulen.
+		this.name = new SimpleStringProperty(name);
+		this.startDate = new SimpleObjectProperty<>(startDate);
+		this.endDate = new SimpleObjectProperty<>(endDate);
+		this.modules = new SimpleListProperty<>();
 	}
 
+	/**
+	 * Die Methode fügt ein Modul zum Semester hinzu.
+	 * @param module Das Modul, welches hinzugefügt werden soll.
+	 */
 	public void addModule(Module module) {
-
+		this.modules.add(module);
 	}
 
+	/**
+	 * Die Methode entfernt ein Modul aus dem Semester.
+	 * @param module Das Modul, welches entfernt werden soll.
+	 */
 	public void removeModule(Module module) {
-
+		this.modules.remove(module);
 	}
 
-	public boolean isModuleinSemester(Module module) {
-		return false;
+	/**
+	 * Methode zum Überprüfen, ob ein Modul bereits dem Semester
+	 * zugeordnet ist.
+	 * @param module Das Modul, welches überprüft werden soll.
+	 * @return True, wenn das Modul bereits dem Semester zugeordet ist,
+	 * sonst false.
+	 */
+	public boolean isModuleInSemester(Module module) {
+		return this.modules.contains(module);
 	}
 
+	//Getter- und Setter-Methoden für die einzelnen Attribut-Werte und Getter-Methode für die Properties
+	//(autogeneriert mit Intellij)
+	public String getName() {
+		return name.get();
+	}
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name.set(name);
+	}
+
+	public LocalDate getStartDate() {
+		return startDate.get();
+	}
+
+	public ObjectProperty<LocalDate> startDateProperty() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate.set(startDate);
+	}
+
+	public LocalDate getEndDate() {
+		return endDate.get();
+	}
+
+	public ObjectProperty<LocalDate> endDateProperty() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate.set(endDate);
+	}
+
+	public ObservableList<Module> getModules() {
+		return modules.get();
+	}
+
+	public ListProperty<Module> modulesProperty() {
+		return modules;
+	}
+
+	public void setModules(ObservableList<Module> modules) {
+		this.modules.set(modules);
+	}
 }
