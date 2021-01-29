488ebf1f7a76b2ad19097ba74385db69a9f261c1
Christian Riest <christian.riest@cs.tu-dortmund.de>
2021-01-29 14:26:41 +0100

    Exception-Klassen für Fehlerbehandlung hinzugefügt

---
+++ src/exceptions/DataNotValidException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+/**
+ * Exception-Klasse, die eine falsche/ungültige Eingabe des Benutzers repräsentiert.
+ */
+public class DataNotValidException extends Exception{
+}
---
+++ src/exceptions/ModuleAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+/**
+ * Exception-Klasse, die repräsentiert, dass ein Modul bereits existiert.
+ */
+public class ModuleAlreadyExistsException extends Exception{
+}
