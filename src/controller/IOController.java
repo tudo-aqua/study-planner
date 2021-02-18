c7127059186f25512b5b5eecd6732e4edee5f7d7
Christian Riest <christian.riest@cs.tu-dortmund.de>
2021-02-18 11:55:29 +0100

    JavaDoc für IOController ergänzt

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -1,11 +1,21 @@
 package controller;
 
+/**
+ * Die Klasse stellt Methoden für das Laden und Speichern der Programmdaten zur Verfügung.
+ */
 public class IOController {
 
+	/**
+	 * Referenz auf den zentralen Controller der Controller-Schicht.
+	 */
 	private StudyPlannerController studyPlannerController;
 
+	/**
+	 * Konstruktor, der den StudyPlannerController setzt.
+	 * @param spc Referenzu auf den zentralen StudyPlannerController.
+	 */
 	public IOController(StudyPlannerController spc) {
-
+		this.studyPlannerController = spc;
 	}
 
 	public void loadData() {
