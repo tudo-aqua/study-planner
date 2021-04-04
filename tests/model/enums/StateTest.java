fa987a7f8baff7a80dc03d287f73753ef9d29f06
Christian Riest <christian.riest@cs.tu-dortmund.de>
2021-04-04 23:28:40 +0200

    Test für Enum State erstellt

---
+++ tests/model/enums/StateTest.java
@@ -0,0 +1,36 @@
+package model.enums;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *  * Testklasse für die Enum State.
+ */
+public class StateTest {
+
+    /**
+     * Test für stateFromId-Methode des State-Enums.
+     */
+    @Test
+    public void stateFromId() {
+        assertEquals(State.PASSED_WITH_GRADE,State.stateFromId(1));
+        assertEquals(State.PASSED_WITHOUT_GRADE,State.stateFromId(2));
+        assertEquals(State.NOT_PASSED,State.stateFromId(3));
+        assertEquals(State.NO_RESULT,State.stateFromId(4));
+        assertEquals(State.NO_RESULT,State.stateFromId(42));
+    }
+
+
+    /**
+     * Test für getId-Methode des State-Enums.
+     */
+    @Test
+    public void getId() {
+        assertEquals(1,State.PASSED_WITH_GRADE.getId());
+        assertEquals(2,State.PASSED_WITHOUT_GRADE.getId());
+        assertEquals(3,State.NOT_PASSED.getId());
+        assertEquals(4,State.NO_RESULT.getId());
+    }
+}
